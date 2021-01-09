0cd5655e54cdd779be9139514c24824817f100c2
Tuojian Lyu <tuojianlyu@gmail.com>
2021-01-09 16:57:57 +0200

    Singleton example

--- src/main/Demo.java
+++ src/main/Demo.java
@@ -1,52 +1,15 @@
 package src.main;
 
-import src.shapes.Circle;
-import src.shapes.Rectangle;
-import src.shapes.Shape;
-
-import java.util.ArrayList;
-import java.util.List;
+import src.singleton.Singleton;
 
 public class Demo {
     public static void main(String[] args) {
-        List<Shape> shapes = new ArrayList<>();
-        List<Shape> shapesCopy = new ArrayList<>();
-
-        Circle circle = new Circle();
-        circle.x = 10;
-        circle.y = 20;
-        circle.radius = 15;
-        circle.color = "red";
-        shapes.add(circle);
-
-        Circle anotherCircle = (Circle) circle.clone();
-        shapes.add(anotherCircle);
-
-        Rectangle rectangle = new Rectangle();
-        rectangle.width = 10;
-        rectangle.height = 20;
-        rectangle.color = "blue";
-        shapes.add(rectangle);
-
-        cloneAndCompare(shapes, shapesCopy);
-    }
-
-    private static void cloneAndCompare(List<Shape> shapes, List<Shape> shapesCopy) {
-        for (Shape shape : shapes) {
-            shapesCopy.add(shape.clone());
-        }
-
-        for (int i = 0; i < shapes.size(); i++) {
-            if (shapes.get(i) != shapesCopy.get(i)) {
-                System.out.println(i + ": Shapes are different objects (yay!)");
-                if (shapes.get(i).equals(shapesCopy.get(i))) {
-                    System.out.println(i + ": And they are identical (yay!)");
-                } else {
-                    System.out.println(i + ": But they are not identical (booo!)");
-                }
-            } else {
-                System.out.println(i + ": Shape objects are the same (booo!)");
-            }
-        }
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+        Singleton singleton = Singleton.getInstance("FOO");
+        Singleton anotherSingleton = Singleton.getInstance("BAR");
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
     }
 }
